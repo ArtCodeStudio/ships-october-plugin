bcd08786f230f435ff41ec88083bf34b32f3322e
Moritz Raguschat <moritz@jumplink.me>
2020-11-06 19:33:02 +0700

    add 'container capacity' field, show it in ships list instead of cranes with priority

---
+++ updates/builder_table_update_jumplink_ships_ships.php
@@ -0,0 +1,23 @@
+<?php namespace JumpLink\Ships\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateJumplinkShipsShips extends Migration
+{
+    public function up()
+    {
+        Schema::table('jumplink_ships_ships', function($table)
+        {
+            $table->string('cc', 191)->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('jumplink_ships_ships', function($table)
+        {
+            $table->dropColumn('cc');
+        });
+    }
+}
